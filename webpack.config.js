9c490acdd73d3a9055c664cda76cb738a5a7c7d2
Oksana Vdovenko <OKVD@Oksanas-MacBook-Air.local>
2017-09-19 19:30:07 +0300

    webpack.config adjusted

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ module.exports = {
   context: __dirname,
   entry: {
     jsx: "./src/index.jsx",
-    css: "./src/main.css",
-    html: "./src/index.html",
+    // css: "./src/main.css",
+    // html: "./src/index.html",
   },
 
   output: {
@@ -11,16 +11,56 @@ module.exports = {
     filename: "bundle.js",
   },
   module: {
+    rules: [
+      {
+        test: /\.js$/, //Check for all js files
+        loader: 'babel-loader',
+        query: {
+          presets: [ "babel-preset-env" ].map(require.resolve)
+        }
+      },
+      // {
+      //   test: /\.(sass|scss)$/, //Check for sass or scss file names
+      //   use: [
+      //     'style-loader',
+      //     'css-loader',
+      //     'sass-loader',
+      //   ]
+      // },
+      // {
+      //   test: /\.json$/,
+      //   loader: "json-loader"  //JSON loader
+      // }
+    ]
+  },
+  //To run development server
+  devServer: {
+    contentBase: __dirname + '/src',
+  },
+  // module: {
     // preLoaders: [
     //     //Eslint loader
     //   { test: /\.jsx?$/, exclude: /node_modules/, loader: "eslint-loader"},
     // ],
-    loaders: [
-      { test: /\.html$/, loader: "file?name=[name].[ext]" },
-      { test: /\.css$/, loader: "file?name=[name].[ext]" },
-      { test: /\.jsx?$/, exclude: /node_modules/, loaders: ["react-hot","babel-loader"]},
-    ],
-  },
+  //   rules: [
+  //     { test: /\.html$/,
+  //       use: [
+  //         {
+  //           loader: "file?name=[name]-loader.[ext]"
+  //         }
+  //       ]
+  //     }
+  //     { test: /\.css$/,
+  //       use: [
+  //         {
+  //           loader: "file?name=[name]-loader.[ext]"
+  //         }
+  //       ]
+  //     },
+  //     { test: /\.jsx?$/,
+  //       exclude: /node_modules/, loaders: ["react-hot","babel-loader"]},
+  //   ],
+  // },
   // resolve: {
   //   extensions: ['', '.js', '.jsx']
   // },
